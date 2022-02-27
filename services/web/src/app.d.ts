74121e888df9b2e83929922a83153b774257dc23
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2022-02-27 03:11:57 +0000

    fix app.d.ts

--- services/web/src/app.d.ts
+++ services/web/src/app.d.ts
@@ -1,12 +1,9 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            NODE_ENV: "development" | "production"
-            DISCORD_AUTH_TOKEN: string
-            DISCORD_CLIENT_ID: string
+            NODE_ENV?: "development" | "production"
             HASH_TOKEN: string
-            AWS_ACCESS_KEY_ID: string
-            AWS_SECRET_ACCESS_KEY: string
+            DB_URL: string
         }
     }
 }
